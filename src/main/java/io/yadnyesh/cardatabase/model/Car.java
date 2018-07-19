9d674ea5d85d39abefafc35d5148ec08cf704fb9
Unknown <yadnyesh.juvekar@gmail.com>
2018-07-19 21:44:22 +0530

    Enabled JPA and H2 Database

--- src/main/java/io/yadnyesh/cardatabase/model/Car.java
+++ src/main/java/io/yadnyesh/cardatabase/model/Car.java
@@ -1,7 +1,90 @@
 package io.yadnyesh.cardatabase.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by yadnyesh on 19/7/18.
  */
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String brand;
+    private String model;
+    private String color;
+    private String registerNumber;
+    private int year;
+    private int price;
+
+    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    }
+
+    public Car() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
