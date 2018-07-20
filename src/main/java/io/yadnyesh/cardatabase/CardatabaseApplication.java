044c784e560ad0c3844b6c1f90e2e9cafbf77c4b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-20 12:22:40 +0530

    Added sample data at start of the application

--- src/main/java/io/yadnyesh/cardatabase/CardatabaseApplication.java
+++ src/main/java/io/yadnyesh/cardatabase/CardatabaseApplication.java
@@ -1,7 +1,9 @@
 package io.yadnyesh.cardatabase;
 
 import io.yadnyesh.cardatabase.model.Car;
+import io.yadnyesh.cardatabase.model.Owner;
 import io.yadnyesh.cardatabase.repository.CarRepository;
+import io.yadnyesh.cardatabase.repository.OwnerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,9 @@ public class CardatabaseApplication {
 	@Autowired
 	CarRepository carRepository;
 
+	@Autowired
+	OwnerRepository ownerRepository;
+	
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -25,10 +30,15 @@ public class CardatabaseApplication {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-//			carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1211", 2017, 29000));
-//			carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3001", 2014, 59000));
-//			carRepository.save(new Car("Toyota", "Prius", "Silver", "TOT-2002", 2018, 19000));
-//			carRepository.save(new Car("Maruti", "Dzire", "Blue", "GA0-8936", 2010, 49000));
+			Owner owner1 = new Owner("Yadnyesh", "Juvekar");
+			Owner owner2 = new Owner("Bindiya", "Juvekar");
+			ownerRepository.save(owner1);
+			ownerRepository.save(owner2);
+			
+			carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1211", 2017, 29000, owner1));
+			carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3001", 2014, 59000, owner1));
+			carRepository.save(new Car("Toyota", "Prius", "Silver", "TOT-2002", 2018, 19000, owner2));
+			carRepository.save(new Car("Maruti", "Dzire", "Blue", "GA0-8936", 2010, 49000, owner2));
 		};
 	}
 }
