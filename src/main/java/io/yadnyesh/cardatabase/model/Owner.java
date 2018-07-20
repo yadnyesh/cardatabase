0ec449300207ea9f84a6489625ac0a9d22d5b91f
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-20 08:54:34 +0530

    Added Owner Model

---
+++ src/main/java/io/yadnyesh/cardatabase/model/Owner.java
@@ -0,0 +1,48 @@
+package io.yadnyesh.cardatabase.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Owner {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long ownerid;
+	private String firstname;
+	private String lastName;
+	
+	public Owner() {
+	}
+	
+	public Owner(String firstname, String lastName) {
+		this.firstname = firstname;
+		this.lastName = lastName;
+	}
+	
+	public long getOwnerid() {
+		return ownerid;
+	}
+	
+	public void setOwnerid(long ownerid) {
+		this.ownerid = ownerid;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
