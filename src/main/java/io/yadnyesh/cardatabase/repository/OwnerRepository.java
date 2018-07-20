27bfad183d630d2147fdab29edf56ff443e9ac57
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-20 09:10:10 +0530

    Added OwnerRepository

---
+++ src/main/java/io/yadnyesh/cardatabase/repository/OwnerRepository.java
@@ -0,0 +1,8 @@
+package io.yadnyesh.cardatabase.repository;
+
+import io.yadnyesh.cardatabase.model.Owner;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OwnerRepository extends PagingAndSortingRepository<Owner, Long> {
+
+}
