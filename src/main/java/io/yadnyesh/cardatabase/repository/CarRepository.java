84620a2cf7e314cd988b996f8c3d62059eae4517
Unknown <yadnyesh.juvekar@gmail.com>
2018-07-20 07:00:22 +0530

    Added single parameter methods

--- src/main/java/io/yadnyesh/cardatabase/repository/CarRepository.java
+++ src/main/java/io/yadnyesh/cardatabase/repository/CarRepository.java
@@ -3,8 +3,16 @@ package io.yadnyesh.cardatabase.repository;
 import io.yadnyesh.cardatabase.model.Car;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by yadnyesh on 19/7/18.
  */
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    List<Car> findByBrand(String brand);
+
+    List<Car> findByColor(String color);
+
+    List<Car> findByYear(int year);
 }
