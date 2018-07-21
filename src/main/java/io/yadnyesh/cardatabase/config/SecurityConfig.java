b1a45897bcb9b7ba0b1f45f0cbea8b4ba5733226
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-21 18:31:25 +0530

    Added the configure method

--- src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package io.yadnyesh.cardatabase.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
