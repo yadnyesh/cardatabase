c4e402eb770bd24507efd7adc85a70c3327720e1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-21 18:30:34 +0530

    Overriding Web security by overriding WebSecurity

--- src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
@@ -1,4 +1,9 @@
 package io.yadnyesh.cardatabase.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 }
