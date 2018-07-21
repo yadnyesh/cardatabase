24a086ae3d7572fdf10239892e574144e064032a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-21 18:37:28 +0530

    Added UserId field

--- src/main/java/io/yadnyesh/cardatabase/model/User.java
+++ src/main/java/io/yadnyesh/cardatabase/model/User.java
@@ -1,4 +1,12 @@
 package io.yadnyesh.cardatabase.model;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
 }
