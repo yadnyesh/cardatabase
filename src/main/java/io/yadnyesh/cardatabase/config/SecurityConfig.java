e91c18178f7c7f937c1cb3244171942684b1b3ca
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-21 18:34:35 +0530

    Added UserDetailsService to have in-memory users.

--- src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+    }
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                                        .username("user")
+                                        .password("password")
+                                        .roles("USER")
+                                        .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
