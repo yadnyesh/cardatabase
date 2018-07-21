41a16bf5d5fdcbedb2128c37bfb6c2d1d5ad6208
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-21 18:41:18 +0530

    Added Getters and Setters

--- src/main/java/io/yadnyesh/cardatabase/model/User.java
+++ src/main/java/io/yadnyesh/cardatabase/model/User.java
@@ -29,5 +29,35 @@ public class User {
         this.role = role;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
