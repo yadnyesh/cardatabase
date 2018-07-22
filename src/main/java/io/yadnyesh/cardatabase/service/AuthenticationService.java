1bc92c0ed9a57896ecaff050616d37cc4a78d3d2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 23:31:19 +0530

    Define SIGNINGKEY

--- src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
@@ -3,6 +3,7 @@ package io.yadnyesh.cardatabase.service;
 public class AuthenticationService {
 
     static final long EXPIRATIONTIME = 864_000_00;
+    static final String SIGNINGKEY = "SecretKey";
 
 
 }
