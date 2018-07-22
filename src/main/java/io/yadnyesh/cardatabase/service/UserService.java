13661342bddeb5b66e18ebaf3cd4a35a063335f6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 12:47:32 +0530

    Create filed for Injecting UserRepository

--- src/main/java/io/yadnyesh/cardatabase/service/UserService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/UserService.java
@@ -1,5 +1,8 @@
 package io.yadnyesh.cardatabase.service;
 
+import io.yadnyesh.cardatabase.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +10,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService{
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        UserDetails user = new User(userName, currentUser.get)
         return null;
     }
+
 }
