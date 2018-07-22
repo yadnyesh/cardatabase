690f0cc28aa2c4b26b28f36a26d9fc32e7a24505
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 23:31:46 +0530

    Define PREFIX

--- src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
@@ -4,6 +4,7 @@ public class AuthenticationService {
 
     static final long EXPIRATIONTIME = 864_000_00;
     static final String SIGNINGKEY = "SecretKey";
+    static final String PREFIX = "Bearer";
 
 
 }
