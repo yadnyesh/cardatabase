31e860ebcdc51ef874f0fa3e003049860cf4fc33
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 23:33:04 +0530

    Added the static addToken skeleton

--- src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
@@ -1,10 +1,15 @@
 package io.yadnyesh.cardatabase.service;
 
+import javax.servlet.http.HttpServletResponse;
+
 public class AuthenticationService {
 
     static final long EXPIRATIONTIME = 864_000_00;
     static final String SIGNINGKEY = "SecretKey";
     static final String PREFIX = "Bearer";
 
+    //Add Token to the Authorization Header
+    static public void addToken(HttpServletResponse httpServletResponse, String userName) {
 
+    }
 }
