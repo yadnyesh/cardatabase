41fd0589e8c9729984f8265dd746f19134b89991
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 18:25:32 +0530

    Repository Annotation required.

--- src/main/java/io/yadnyesh/cardatabase/repository/UserRepository.java
+++ src/main/java/io/yadnyesh/cardatabase/repository/UserRepository.java
@@ -2,7 +2,9 @@ package io.yadnyesh.cardatabase.repository;
 
 import io.yadnyesh.cardatabase.model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>{
 
     User findByUserName(String userName);
