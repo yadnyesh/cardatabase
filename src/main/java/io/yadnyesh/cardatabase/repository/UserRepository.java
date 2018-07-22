73391120caadb379e2b80a707ec656fd922799c7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 07:29:45 +0530

    Added findByUserName method

--- src/main/java/io/yadnyesh/cardatabase/repository/UserRepository.java
+++ src/main/java/io/yadnyesh/cardatabase/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package io.yadnyesh.cardatabase.repository;
 
-public class UserRepository {
+import io.yadnyesh.cardatabase.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+    User findByUserName(String userName);
 }
