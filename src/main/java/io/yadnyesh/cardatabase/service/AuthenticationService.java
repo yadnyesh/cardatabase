f60acfbc4457fd238a79568c572121abe4e651ed
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 23:53:17 +0530

    First read Authorization header

--- src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 
@@ -23,7 +24,7 @@ public class AuthenticationService {
     }
 
     //Get token from Authorization header
-    static public Authentication getAuthentication() {
-
+    static public Authentication getAuthentication(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader("Authorization");
     }
 }
