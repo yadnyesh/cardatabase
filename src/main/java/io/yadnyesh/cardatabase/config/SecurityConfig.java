0e713e8a862ca8ffebec1cbd19a55eb6032afa18
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 13:02:00 +0530

    Added the configureGlobal method by overriding it.

--- src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package io.yadnyesh.cardatabase.config;
 import io.yadnyesh.cardatabase.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -29,5 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //        return new InMemoryUserDetailsManager(userDetails);
 //    }
 
-
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 }
