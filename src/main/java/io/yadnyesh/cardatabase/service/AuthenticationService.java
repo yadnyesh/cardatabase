a6143d9242b7ee51b523bb3227f47b47b42895aa
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 23:50:01 +0530

    Add Authorization header

--- src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
@@ -17,6 +17,6 @@ public class AuthenticationService {
         String JwtToken = Jwts.builder().setSubject(userName)
                         .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
                         .signWith(SignatureAlgorithm.ES512, SIGNINGKEY).compact();
-        
+        httpServletResponse.addHeader("Authorization", PREFIX + " " + JwtToken);
     }
 }
