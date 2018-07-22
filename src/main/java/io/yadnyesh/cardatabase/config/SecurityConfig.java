23584a0252f955a6665113152bc90ed6f7e66153
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 12:59:32 +0530

    Autowire - UserDetailsServiceImpl

--- src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.yadnyesh.cardatabase.config;
 
+import io.yadnyesh.cardatabase.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
