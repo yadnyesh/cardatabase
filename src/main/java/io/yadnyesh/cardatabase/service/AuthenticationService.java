dacf089490a226c0f13436ec14e87acda4452076
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 23:44:46 +0530

    Build JwtToken

--- src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/AuthenticationService.java
@@ -1,6 +1,10 @@
 package io.yadnyesh.cardatabase.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 public class AuthenticationService {
 
@@ -10,5 +14,8 @@ public class AuthenticationService {
 
     //Add Token to the Authorization Header
     static public void addToken(HttpServletResponse httpServletResponse, String userName) {
+        String JwtToken = Jwts.builder().setSubject(userName)
+                        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                        .signWith(SignatureAlgorithm.ES512, SIGNINGKEY).compact();
     }
 }
