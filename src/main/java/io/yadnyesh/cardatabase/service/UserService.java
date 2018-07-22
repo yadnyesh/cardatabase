90b2df5aeb5f57fc51185a1ec0c45168680d5a4b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-22 12:45:16 +0530

    Override UserDetailsService the interface provided by Spring security

--- src/main/java/io/yadnyesh/cardatabase/service/UserService.java
+++ src/main/java/io/yadnyesh/cardatabase/service/UserService.java
@@ -1,7 +1,14 @@
 package io.yadnyesh.cardatabase.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
