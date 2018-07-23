23f4ce0c2d068f2f39c89e12175645473dd5a9cd
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 12:09:10 +0530

    New Constructor - New car without Owner

--- src/main/java/io/yadnyesh/cardatabase/model/Car.java
+++ src/main/java/io/yadnyesh/cardatabase/model/Car.java
@@ -30,7 +30,17 @@ public class Car {
     @JoinColumn(name="owner")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Owner owner;
-
+    
+    public Car(String brand, String model, String color, String registerNumber, int year, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    }
+    
+    
     public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
         this.brand = brand;
         this.model = model;
