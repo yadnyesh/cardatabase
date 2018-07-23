c48a0747a89dbabb69bb44d40b3688cdb3a214a5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 10:42:40 +0530

    Autowire CarController

--- src/test/java/io/yadnyesh/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/io/yadnyesh/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,18 @@
 package io.yadnyesh.cardatabase;
 
+import io.yadnyesh.cardatabase.Controller.CarController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CardatabaseApplicationTests {
+	
+	@Autowired
+	private CarController carController;
 
 	@Test
 	public void contextLoads() {
