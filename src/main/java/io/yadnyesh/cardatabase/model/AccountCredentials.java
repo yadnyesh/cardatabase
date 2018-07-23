7ae7a9b575a8f9d951a412adfa615159f64ac0a8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 08:43:41 +0530

    Adding attributes and constructor.

--- src/main/java/io/yadnyesh/cardatabase/model/AccountCredentials.java
+++ src/main/java/io/yadnyesh/cardatabase/model/AccountCredentials.java
@@ -1,3 +1,16 @@
 package io.yadnyesh.cardatabase.model;
 
-public class AccountCredentials {}
+public class AccountCredentials {
+	private String userName;
+	private String password;
+	
+	public AccountCredentials() {
+	}
+	
+	public AccountCredentials(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+	
+	
+}
