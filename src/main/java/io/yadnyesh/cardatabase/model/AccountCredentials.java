175373c9038a81ebed353d84b401918c45491537
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 08:48:20 +0530

    Added the getter and setter methods

--- src/main/java/io/yadnyesh/cardatabase/model/AccountCredentials.java
+++ src/main/java/io/yadnyesh/cardatabase/model/AccountCredentials.java
@@ -12,5 +12,19 @@ public class AccountCredentials {
 		this.password = password;
 	}
 	
+	public String getUserName() {
+		return userName;
+	}
 	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
