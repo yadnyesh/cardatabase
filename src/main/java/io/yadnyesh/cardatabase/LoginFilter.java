2db843a9abb1f4e70204b7dafacc8f1b44bf45f5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 09:06:06 +0530

    Return the AuthenticationDetails

--- src/main/java/io/yadnyesh/cardatabase/LoginFilter.java
+++ src/main/java/io/yadnyesh/cardatabase/LoginFilter.java
@@ -3,6 +3,7 @@ package io.yadnyesh.cardatabase;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.yadnyesh.cardatabase.model.AccountCredentials;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -12,8 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-
+import java.util.Collections;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 	
@@ -27,5 +27,10 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 			throws AuthenticationException, IOException, ServletException {
 		AccountCredentials accountCredentials = new ObjectMapper()
 												.readValue(httpServletRequest.getInputStream(), AccountCredentials.class);
+		return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(
+											accountCredentials.getUserName(),
+				                            accountCredentials.getPassword(),
+											Collections.emptyList())
+		);
 	}
 }
