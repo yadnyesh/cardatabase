5649bed3a1df26ff621b244a87d0bf06be02c296
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 15:35:29 +0530

    Creating class for User Login

--- src/test/java/io/yadnyesh/cardatabase/UserLoginUnitTest.java
+++ src/test/java/io/yadnyesh/cardatabase/UserLoginUnitTest.java
@@ -1,3 +1,13 @@
 package io.yadnyesh.cardatabase;
 
-public class UserLoginUnitTest {}
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserLoginUnitTest {
+
+}
