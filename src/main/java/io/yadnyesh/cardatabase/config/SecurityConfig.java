6b86382398f8c6c4dcc7032cbad2c0c0bd00e97a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 09:47:35 +0530

    Implement - override the configure method

--- src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/cardatabase/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package io.yadnyesh.cardatabase.config;
 
+import io.yadnyesh.cardatabase.LoginFilter;
 import io.yadnyesh.cardatabase.repository.UserRepository;
 import io.yadnyesh.cardatabase.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +28,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.cors().and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new LoginFilter("/login", authenticationManager()),
+                                                    UsernamePasswordAuthenticationFilter.class);
     }
 }
