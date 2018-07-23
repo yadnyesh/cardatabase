ebff13b4d22edbd417141894b963735cdd599e0f
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 12:04:33 +0530

    Inject 	TestEntityManager entityManager;

--- src/test/java/io/yadnyesh/cardatabase/CarRepositoryUnitTest.java
+++ src/test/java/io/yadnyesh/cardatabase/CarRepositoryUnitTest.java
@@ -1,11 +1,15 @@
 package io.yadnyesh.cardatabase;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class )
 @DataJpaTest
 public class CarRepositoryUnitTest {
 
+	@Autowired
+	TestEntityManager entityManager;
 }
