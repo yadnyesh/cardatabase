fff22879ff780d9968df3f79601b105783e9911a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 12:04:05 +0530

    DataJpaTest can be used if the test only focusses on JPA testing

--- src/test/java/io/yadnyesh/cardatabase/CarRepositoryUnitTest.java
+++ src/test/java/io/yadnyesh/cardatabase/CarRepositoryUnitTest.java
@@ -1,10 +1,11 @@
 package io.yadnyesh.cardatabase;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class )
-
+@DataJpaTest
 public class CarRepositoryUnitTest {
 
 }
