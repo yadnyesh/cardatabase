1606264f5c9e4f97f2f6799c5ad487e1543c0f20
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 12:03:31 +0530

    Mark to run the tests with SpringRunner

--- src/test/java/io/yadnyesh/cardatabase/CarRepositoryUnitTest.java
+++ src/test/java/io/yadnyesh/cardatabase/CarRepositoryUnitTest.java
@@ -1,5 +1,10 @@
 package io.yadnyesh.cardatabase;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class )
+
 public class CarRepositoryUnitTest {
 
 }
