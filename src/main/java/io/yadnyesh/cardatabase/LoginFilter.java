6712d20d24510022d1180b52572a7fd5649c61eb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 08:57:19 +0530

    Override attemptAuthentication in LoginFilter

--- src/main/java/io/yadnyesh/cardatabase/LoginFilter.java
+++ src/main/java/io/yadnyesh/cardatabase/LoginFilter.java
@@ -1,7 +1,18 @@
 package io.yadnyesh.cardatabase;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
-public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+		return null;
+	}
 }
