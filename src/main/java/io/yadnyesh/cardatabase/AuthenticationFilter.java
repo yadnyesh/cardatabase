d730d2a9259fa6afeecba40236dfc8687e20b38d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 09:40:27 +0530

    Get the authentication

--- src/main/java/io/yadnyesh/cardatabase/AuthenticationFilter.java
+++ src/main/java/io/yadnyesh/cardatabase/AuthenticationFilter.java
@@ -1,17 +1,23 @@
 package io.yadnyesh.cardatabase;
 
+import io.yadnyesh.cardatabase.service.AuthenticationService;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class AuthenticationFilter extends GenericFilterBean {
 	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		Authentication authentication = AuthenticationService.getAuthentication( (HttpServletRequest) request);
+		
 	
 	}
 }
