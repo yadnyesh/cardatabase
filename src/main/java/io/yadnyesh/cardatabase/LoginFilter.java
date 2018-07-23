b4dc1d451adb95cde8dbe1e689a7e37890893da0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-07-23 08:58:26 +0530

    Adding constructor for LoginFilter

--- src/main/java/io/yadnyesh/cardatabase/LoginFilter.java
+++ src/main/java/io/yadnyesh/cardatabase/LoginFilter.java
@@ -1,8 +1,10 @@
 package io.yadnyesh.cardatabase;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+	
+	public LoginFilter(String url, AuthenticationManager authenticationManager) {
+		super(new AntPathRequestMatcher(url));
+	}
 	
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
